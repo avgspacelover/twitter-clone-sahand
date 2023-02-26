248126f47c2b4d67f01e8dc4ba8651be0edb4c1a
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2023-02-26 17:57:11 +0530

    feed header done

---
+++ twitter-clone/components/Feed.js
@@ -0,0 +1,16 @@
+import { SparklesIcon } from '@heroicons/react/outline'
+import React from 'react'
+
+export const Feed = () => {
+  return (
+    <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
+        <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+            <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
+            <div className="flex items-center justify-center px-0 ml-auto w-9 h-9 hoverEffect">
+                <SparklesIcon className="h-5"/>
+            </div>
+        </div>
+        Home
+    </div>
+  )
+}
