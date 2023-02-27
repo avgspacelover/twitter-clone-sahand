34f63e05e478cd27117450f91200dac18a3bdbc9
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2023-02-27 10:03:24 +0530

    whats happening section in widgets UI done

--- twitter-clone/components/Widgets.js
+++ twitter-clone/components/Widgets.js
@@ -1,7 +1,11 @@
 import { SearchIcon } from "@heroicons/react/outline"
+import { useState } from "react"
+import { News } from "./News"
 
 
-export const Widgets = () => {
+export const Widgets = ({newsResults}) => {
+
+    const [artNum, setArtNum]= useState(3)
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
         <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-52  ">
@@ -12,6 +16,20 @@ export const Widgets = () => {
 
 
         </div>
+        <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2">
+            <h4 className="font-bold text-xl px-4 ">
+                What's Happening?
+
+            </h4>
+            {newsResults.slice(0,artNum).map((articles)=> (
+                <News key={articles.title} article={article}/>
+            ))}
+
+            <button onClick={()=>setArtNum(artNum+3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
+                Show More
+            </button>
+        </div>
+        
     </div>
   )
 }
--- twitter-clone/pages/index.js
+++ twitter-clone/pages/index.js
@@ -5,7 +5,7 @@ import { Widgets } from '../components/Widgets'
 
 
 
-export default function Home() {
+export default function Home({newsResults}) {
   return (
     <div>
       <Head>
@@ -19,11 +19,25 @@ export default function Home() {
 
        <Feed />
 
-       <Widgets />
+       <Widgets newsResults={newsResults.articles} />
 
       </main>
 
 
     </div>
   )
+}
+
+
+// https://saurav.tech/NewsAPI/top-headlines/category/business/in.json
+
+export async function getServerSideProps(){
+
+  const newsResults= await fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/in.json").then((res)=> res.json());
+
+  return {
+    props:{
+      newsResults,
+    }
+  };
 }
